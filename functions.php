4255ae78d6b4ccbbba81cf204523d782b2e0f9f8
BasselAlli96 <121745197+BasselAlli96@users.noreply.github.com>
2025-05-18 17:08:16 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -82,14 +82,13 @@ function load_theme_parts($context) {
                 'template' => 'hero-section',
                 'load_css' => false,
                 'load_js' => false,
-            ],
-        'features-section' => [
+            ] ,
             'features-part' => [
                 'template' => 'services_section',
                 'load_css' => false,
                 'load_js' => false,
-            ]
+                ]
         ]
-    ]];
+];
     return apply_filters("theme_sections_{$context}", $registry[$context] ?? []);
 }
