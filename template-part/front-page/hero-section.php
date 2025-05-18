c30887210375ab9f46ce26238c72779e4af8f782
BasselAlli96 <121745197+BasselAlli96@users.noreply.github.com>
2025-05-18 14:29:53 +0300

    Create hero-section.php

---
+++ template-part/front-page/hero-section.php
@@ -0,0 +1,40 @@
+<?php 
+/***
+ * 
+ * FRONT PAGE TEMPLATE
+ * 
+ * 
+ */
+get_header();
+?>
+<section id="main-page" class="main-page">
+    
+    <div id="main-front" class="front-hero">
+
+        <div class="hero-container">
+            <!-- three hiden layers (except active class) fot gradient layers -->
+            <div class="gradient-overlay gradient-1 active"></div>
+            <div class="gradient-overlay gradient-2"></div>
+            <div class="gradient-overlay gradient-3"></div>
+            <div class="hero-text">
+                <span class="hero-bold-text">
+                    أدخل إلى عالم
+                    من الفن والإبداع
+                    مع استديو أرتوبيا     
+                </span>
+
+                <span class="hero-light-text">
+                                    حول محيطك إلى فضاء من الفن والجمال
+                                    عبر لوحات الريزن الفنية
+
+                </span>
+            </div>
+            <div class="hero-images">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/1.png " class="active" style="z-index: 3">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/2.png " style="z-index: 2">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/3.png " style="z-index: 1">
+            </div>
+
+        </div>
+    </div>
+</section>
