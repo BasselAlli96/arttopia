860f140ea6710ee590f8020614692a81b28d0aaa
BasselAlli96 <bassel.1996.fc@gmail.com>
2025-05-19 14:06:06 +0300

    Add servcies section file

---
+++ template-part/front-page/services_section.php
@@ -0,0 +1,53 @@
+
+<section id="services-part" class="services-section">
+
+  <div class="title_section">
+    <h1>Rami</h1>
+  </div>
+
+  <div class="section-container">
+  
+    <!-- Background Image -->
+    
+      <img  class="section-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/Resin.jpg" alt="">
+    
+      <!-- Three Boxes Overlay -->
+      <div class="box-container">
+        
+        <div class="box-inside">
+      
+            <div class="box box-1 box-button">
+
+              <div class="svg_icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/gift.svg" alt="PS5 Icon" class="box-icon">
+              </div>
+              <h3>Available PS5s</h3>
+              <p>Check real-time status</p>
+            </div>
+            <div class="box box-2 box-button">
+
+              <div class="svg_icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/gift.svg" alt="PS5 Icon" class="box-icon">
+              </div>
+              <h3>Store Locator</h3>
+              <p>Find nearest branch</p>
+
+            </div>
+
+            <div class="box box-3 box-button">
+              <div class="svg_icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero/gift.svg" alt="PS5 Icon" class="box-icon"></div>
+
+              <h3>Hourly Pricing</h3>
+              <p>ادخل إلى عالم من الابداع
+                مع ارتوبيا حيث نقدم افضل الهدايا التي ترغب بها
+                ادخل الفرحة إلى قلب اصدقائك
+              </p>
+            </div>
+          </div>
+      </div>
+      
+  </div>
+
+</section>
+
+
